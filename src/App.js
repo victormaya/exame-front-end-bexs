9ef64dc2236f6f1ab05dbb01961f31fd9d801812
Victor Maya <victor.maya42@gmail.com>
2021-04-19 19:43:58 -0300

    style: toda a estilização da primeira tela

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
-import { Page, Container, ViewLeft } from "./styled";
+import React, { useEffect, useState } from "react";
+import { Page, Container, ViewLeft, ViewRight, Formulario } from "./styled";
 import seta from "./assets/icons/seta.png";
+import seta_right from "./assets/icons/seta-r.png";
 import icon_card from "./assets/icons/icon-card.svg";
 import cartao from "./assets/icons/cartao.svg";
+import check from "./assets/icons/check.png";
 
 function App() {
+  var currentNumber = ["* * * *", "* * * *", "* * * *", "* * * *"];
+
+  const InserirNumeroCartao = (numero) => {
+    currentNumber = ["* * * *", "* * * *"];
+    // if (numero.length <= 4) {
+    //   currentNumber = [numero.slice(0, numero.length - 1)];
+    // } else if (numero.length > 4 && numero.length <= 8) {
+    //   currentNumber = [numero.slice(0, 4), numero.slice(4, numero.length - 1)];
+    // } else if (numero.length > 8 && numero.length <= 12) {
+    //   currentNumber = [
+    //     numero.slice(0, 4),
+    //     numero.slice(4, 8),
+    //     numero.slice(8, numero.length - 1),
+    //   ];
+    // } else if (numero.length > 12 && numero.length <= 16) {
+    //   currentNumber = [
+    //     numero.slice(0, 4),
+    //     numero.slice(4, 8),
+    //     numero.slice(8, 12),
+    //     numero.slice(12, numero.length - 1),
+    //   ];
+    // }
+  };
+
   return (
     <>
       <Page>
@@ -22,8 +48,62 @@ function App() {
             </div>
             <div className='cartao'>
               <img src={cartao} alt='icone-card' />
+              <div className='numero-cartao'>
+                <p>{currentNumber[0]}</p>
+                <p>{currentNumber[1]}</p>
+                <p>{currentNumber[2]}</p>
+                <p>{currentNumber[3]}</p>
+              </div>
+              <div className='titular-validade'>
+                <p>NOME DO TITULAR</p>
+                <p>00/00</p>
+              </div>
             </div>
           </ViewLeft>
+          <ViewRight>
+            <div className='passo'>
+              <p className='passo-numero'>1</p>
+              <p>Carrinho</p>
+              <div>
+                <img src={seta_right} alt='seta' />
+              </div>
+              <div className='icone-check'>
+                <img src={check} alt='check' />
+              </div>
+              <p>Pagamento</p>
+              <div>
+                <img src={seta_right} alt='seta' />
+              </div>
+              <p className='passo-numero passo-numero_last'>3</p>
+              <p>Confirmação</p>
+            </div>
+            <Formulario>
+              <input
+                className='form-input'
+                type='number'
+                placeholder='Número do cartão'
+                onChange={(e) => InserirNumeroCartao(e.target.value)}
+              />
+              <input
+                className='form-input'
+                type='text'
+                placeholder='Nome (igual ao cartão)'
+              />
+              <div className='validade-cvv'>
+                <input
+                  className='form-input'
+                  type='number'
+                  placeholder='Validade'
+                />
+                <input className='form-input' type='number' placeholder='CVV' />
+              </div>
+              <input
+                className='form-input'
+                type='number'
+                placeholder='Número de parcelas'
+              />
+            </Formulario>
+          </ViewRight>
         </Container>
       </Page>
     </>
--- src/styled.js
+++ src/styled.js
@@ -7,22 +7,27 @@ export const Page = styled.div`
   justify-content: center;
   align-items: center;
 
-  .img {
-    max-width: 100%;
-    box-sizing: border-box;
+  p {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
   }
 `;
 
 export const Container = styled.div`
   width: 1024px;
   height: 595px;
-  background: peru;
+  display: flex;
 `;
 
 export const ViewLeft = styled.div`
   width: 352px;
   height: 100%;
   background: #de4b4b;
+  z-index: 2;
 
   .alterar-forma {
     padding-top: 55px;
@@ -32,7 +37,6 @@ export const ViewLeft = styled.div`
     > p {
       color: #fff;
       margin: 0;
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
   }
 
@@ -48,7 +52,6 @@ export const ViewLeft = styled.div`
       color: #fff;
       margin: 0;
       margin-left: 15px;
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
       font-size: 20px;
       line-height: 24px;
       letter-spacing: -0.01px;
@@ -59,5 +62,110 @@ export const ViewLeft = styled.div`
   .cartao {
     margin-top: 30px;
     padding-left: 68px;
+    position: relative;
+  }
+
+  .numero-cartao {
+    display: flex;
+    font-size: 24px;
+    line-height: 27px;
+    letter-spacing: -0.12px;
+    color: #fff;
+    position: absolute;
+    top: 80px;
+    left: 95px;
+    width: 100%;
+    > p {
+      margin-right: 28px;
+      text-shadow: 0px 1px 2px #000000b3;
+    }
+  }
+
+  .titular-validade {
+    display: flex;
+    font-size: 16px;
+    line-height: 23px;
+    letter-spacing: -0.12px;
+    color: #fff;
+    top: 150px;
+    left: 95px;
+    position: absolute;
+    text-shadow: 0px 1px 2px #000000b3;
+    > p:nth-child(1) {
+      margin-right: 122px;
+      width: 160px;
+    }
+  }
+`;
+
+export const ViewRight = styled.div`
+  width: 672px;
+  height: 100%;
+  /* background: rgba(0, 0, 0, 0.1); */
+  z-index: 1;
+
+  .passo {
+    margin-top: 50px;
+    padding-left: 195px;
+    display: flex;
+    align-items: center;
+    > p {
+      font-size: 13px;
+      line-height: 0px;
+      letter-spacing: -0.01px;
+      margin-left: 8px;
+      margin-right: 24px;
+      color: #de4b4b;
+    }
+    .passo-numero {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: bolder;
+
+      height: 22px;
+      width: 22px;
+      border: 1.3px solid #de4b4b;
+      border-radius: 50%;
+      margin: 0;
+    }
+    .icone-check,
+    .passo-numero_last {
+      margin-left: 24px;
+    }
+  }
+`;
+
+export const Formulario = styled.div`
+  margin-top: 70px;
+  padding-left: 168px;
+
+  .form-input {
+    width: 440px;
+    height: 25px;
+    border: none;
+    border-bottom: 1px solid #c9c9c9;
+    margin-bottom: 62px;
+    ::placeholder {
+      color: #c9c9c9;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-size: 22px;
+    }
+    :focus {
+      box-shadow: 0 0 0 0;
+      border: 0 none;
+      outline: 0;
+      border-bottom: 1px solid #4bde75;
+    }
+  }
+
+  .validade-cvv {
+    display: flex;
+    .form-input {
+      width: 212px;
+      :nth-child(1) {
+        margin-right: 16px;
+      }
+    }
   }
 `;
